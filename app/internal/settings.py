d44a795a853484e9d612db2e0d0994e52a8c7ce9
MichaelPesce <michaelcpesce@gmail.com>
2024-01-25 06:49:15 -0600

    add settings.py with log and image directories

---
+++ app/internal/settings.py
@@ -0,0 +1,34 @@
+"""
+Configuration for the backend
+"""
+from pathlib import Path
+import logging
+from typing import List, Union
+from pydantic import field_validator
+from pydantic_settings import BaseSettings
+
+
+class AppSettings(BaseSettings):
+    log_dir: Union[Path, None] = None
+    img_dir: Union[Path, None] = None
+
+    @field_validator("log_dir")
+    def validate_log_dir(cls, v):
+        if v is None:
+            v = Path.home() / ".uow" / "logs"
+        v.mkdir(parents=True, exist_ok=True)
+        loggingFormat = "[%(levelname)s] %(asctime)s %(name)s (%(filename)s:%(lineno)s): %(message)s"
+        loggingFileHandler = logging.handlers.RotatingFileHandler(
+            v / "orphaned-wells-ui.log", backupCount=2, maxBytes=5000000
+        )
+        logging.basicConfig(
+            level=logging.INFO, format=loggingFormat, handlers=[loggingFileHandler]
+        )
+        return v
+
+    @field_validator("img_dir")
+    def validate_img_dir(cls, v):
+        if v is None:
+            v = Path.home() / ".uow" / "uploaded_images"
+        v.mkdir(parents=True, exist_ok=True)
+        return v
