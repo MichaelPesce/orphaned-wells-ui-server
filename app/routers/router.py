764fd28c153978003031eb56b39d55ccc79d1129
MichaelPesce <michaelcpesce@gmail.com>
2024-01-26 07:31:09 -0600

    add imagehandling file; add functionality to upload documents to google storage

--- app/routers/router.py
+++ app/routers/router.py
@@ -1,5 +1,5 @@
 # import io
-# import os
+import os
 from fastapi import (
     Body,
     Request,
@@ -11,11 +11,13 @@ from fastapi import (
 )
 from fastapi.responses import StreamingResponse, FileResponse
 import logging
+import aiofiles
+import asyncio
 
-# import aiofiles
 # import copy
 
 from app.internal.data_manager import data_manager
+from app.internal.image_handling import convert_tiff, upload_to_google_storage
 
 _log = logging.getLogger(__name__)
 
@@ -60,10 +62,34 @@ async def add_project(request: Request):
 
 
 @router.post("/upload_document")
-async def upload_document(file: UploadFile = File(...)):
+async def upload_document(background_tasks: BackgroundTasks, file: UploadFile = File(...)):
     """
     Fetch project with provided project id
     Return project data
     """
     _log.info(f"uploading document: {file}")
+    output_path = f'{data_manager.app_settings.img_dir}/{file.filename}'
+    filename, file_ext = os.path.splitext(file.filename)
+    ## read document file
+    try:
+        async with aiofiles.open(output_path, 'wb') as out_file:
+            content = await file.read()  # async read
+            await out_file.write(content)
+        if file_ext == '.tif' or file_ext == '.tiff':
+            _log.info(f"converting to png")
+            output_path = convert_tiff(filename, file_ext, data_manager.app_settings.img_dir)
+            file_ext = ".png"
+    except Exception as e:
+        _log.error(f'unable to read image file: {e}')
+    _log.info(f"uploading document to: {output_path}")
+
+    ## upload to cloud storage (this will overwrite any existing files of the same name):
+    background_tasks.add_task(
+        upload_to_google_storage,
+        file_path=output_path,
+        file_name=f"{filename}{file_ext}",
+    )
+    
+    ## send to google doc AI 
+
     return {"response": "success"}
