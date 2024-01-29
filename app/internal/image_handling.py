b7bf35fa5fe4ed82b804b808dfd43289bdffb8fb
MichaelPesce <michaelcpesce@gmail.com>
2024-01-29 08:01:45 -0600

    add function to generate signed url for images in cloud storage

--- app/internal/image_handling.py
+++ app/internal/image_handling.py
@@ -1,11 +1,13 @@
 import os
 import logging
+import datetime
+import json
 import aiofiles
 import aiohttp
 from PIL import Image
 from gcloud.aio.storage import Storage
 from google.api_core.client_options import ClientOptions
-from google.cloud import documentai
+from google.cloud import documentai, storage
 from dotenv import load_dotenv
 
 _log = logging.getLogger(__name__)
@@ -14,6 +16,8 @@ load_dotenv()
 LOCATION = os.getenv("LOCATION")
 PROJECT_ID = os.getenv("PROJECT_ID")
 PROCESSOR_ID = os.getenv("PROCESSOR_ID")
+STORAGE_SERVICE_KEY = os.getenv("STORAGE_SERVICE_KEY")
+os.environ["GCLOUD_PROJECT"] = PROJECT_ID
 
 docai_client = documentai.DocumentProcessorServiceClient(
     client_options=ClientOptions(api_endpoint=f"{LOCATION}-documentai.googleapis.com")
@@ -41,22 +45,7 @@ def convert_tiff(filename, file_ext, output_directory, convert_to=".png"):
         return filepath
 
 
-async def async_upload_to_bucket(
-    blob_name, file_obj, folder="uploads", bucket_name="uploaded_documents_v0"
-):
-    """Upload image file to bucket."""
-    async with aiohttp.ClientSession() as session:
-        storage = Storage(service_file="./internal/creds.json", session=session)
-        status = await storage.upload(bucket_name, f"{folder}/{blob_name}", file_obj)
-        return status["selfLink"]
-
-
-async def upload_to_google_storage(file_path, file_name):
-    async with aiofiles.open(file_path, "rb") as afp:
-        f = await afp.read()
-    url = await async_upload_to_bucket(file_name, f)
-    _log.info(f"uploaded document to cloud storage: {url}")
-
+## Document AI functions
 def process_image(file_path, file_name, mime_type):
     with open(file_path, "rb") as image:
         image_content = image.read()
@@ -101,3 +90,48 @@ def process_image(file_path, file_name, mime_type):
             # "normalized_value": normalized_value,
         }
     return attributes
+
+## Google Cloud Storage Functions
+async def async_upload_to_bucket(
+    blob_name, file_obj, folder="uploads", bucket_name="uploaded_documents_v0"
+):
+    """Upload image file to bucket."""
+    async with aiohttp.ClientSession() as session:
+        storage = Storage(service_file="./internal/creds.json", session=session)
+        status = await storage.upload(bucket_name, f"{folder}/{blob_name}", file_obj)
+        return status["selfLink"]
+
+
+async def upload_to_google_storage(file_path, file_name):
+    async with aiofiles.open(file_path, "rb") as afp:
+        f = await afp.read()
+    url = await async_upload_to_bucket(file_name, f)
+    _log.info(f"uploaded document to cloud storage: {url}")
+
+
+def generate_download_signed_url_v4(filename, bucket_name = "uploaded_documents_v0"):
+    """Generates a v4 signed URL for downloading a blob.
+
+    Note that this method requires a service account key file. You can not use
+    this if you are using Application Default Credentials from Google Compute
+    Engine or from the Google Cloud SDK.
+    To generate STORAGE_SERVICE_KEY, follow steps here:
+    https://docs.gspread.org/en/latest/oauth2.html#for-bots-using-service-account
+    """
+    
+    storage_client = storage.Client.from_service_account_json(STORAGE_SERVICE_KEY)
+
+    # blob_name: path to file in google cloud bucket
+    blob_name = f"uploads/{filename}"
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+
+    url = blob.generate_signed_url(
+        version="v4",
+        # This URL is valid for 15 minutes
+        expiration=datetime.timedelta(minutes=15),
+        # Allow GET requests using this URL.
+        method="GET",
+    )
+
+    return url
