ce19c4b7e0248d203a6b3ce45ee4fe28fb90bee6
MichaelPesce <michaelcpesce@gmail.com>
2025-04-10 12:55:43 -0500

    remove unneeded code

--- app/internal/google_processor_manager.py
+++ app/internal/google_processor_manager.py
@@ -6,25 +6,10 @@ from google.api_core.client_options import ClientOptions
 from google.cloud import documentai
 from google.api_core.exceptions import FailedPrecondition, InvalidArgument
 
-try:
-    if os.getenv("LOCATION") is None:
-        os.environ["GCLOUD_PROJECT"]
-except:
-    from dotenv import load_dotenv
-
-    # loading variables from .env file
-    load_dotenv()
-
 
 _log = logging.getLogger(__name__)
 
 LOCATION = os.getenv("LOCATION")
-PROJECT_ID = os.getenv("PROJECT_ID")
-PROCESSOR_ID = os.getenv("PROCESSOR_ID")
-PROCESSOR_VERSION_ID = os.getenv("PROCESSOR_VERSION_ID")
-STORAGE_SERVICE_KEY = os.getenv("STORAGE_SERVICE_KEY")
-BUCKET_NAME = os.getenv("STORAGE_BUCKET_NAME")
-DIRNAME, FILENAME = os.path.split(os.path.abspath(sys.argv[0]))
 
 docai_client = documentai.DocumentProcessorServiceClient(
     client_options=ClientOptions(api_endpoint=f"{LOCATION}-documentai.googleapis.com"),
@@ -118,65 +103,3 @@ def undeploy_processor_version(RESOURCE_NAME):
     # or if a request is made on a pretrained processor version
     except (FailedPrecondition, InvalidArgument) as e:
         _log.error(e.message)
-
-
-if __name__ == "__main__":
-    """
-    Below I added an example for a potential implementation for deploying the processors
-        at the start of image_hangeling.py,  process_image(). Additionally, consider where
-        to add the undeploying function.
-
-    Using docai_client.processor_version_path in the place of docai_client.processor_path
-        is supposed to direct the document processing to the specified processor version
-        without needing to set the default processor version, which can also be performed
-        though the API if needed.
-    Something to note is that the current default processor version cannot be identifed
-        through the API. Not sure why but the capability was deliberately left out.
-    """
-
-    ## Document AI functions
-    def process_image(
-        file_name,
-        mime_type,
-        rg_id,
-        record_id,
-        processor_id,
-        processor_attributes,
-        data_manager,
-        image_content,
-        reprocessed=False,
-        files_to_delete=[],
-        processor_version_id=None,
-    ):
-        if processor_id is None:
-            _log.info(
-                f"processor id is none, rolling with default processor: {PROCESSOR_ID}"
-            )
-            processor_id = PROCESSOR_ID
-
-        if processor_version_id is None:
-            RESOURCE_NAME = docai_client.processor_path(
-                PROJECT_ID, LOCATION, processor_id
-            )
-            # Which processor version is the default cannot be determined through API
-            _log.info(
-                "processor version id is none, rolling with default processor version."
-            )
-        else:
-            RESOURCE_NAME = docai_client.processor_version_path(
-                PROJECT_ID, LOCATION, processor_id, processor_version_id
-            )
-            deployment = deploy_processor_version(RESOURCE_NAME)
-            if deployment != "DEPLOYED":
-                _log.error(
-                    "Failed to Deploy"
-                    + f"Project: {PROJECT_ID}"
-                    + f"Location: {LOCATION}"
-                    + f"Processor: {processor_id}"
-                    + f"Processor Version: {processor_version_id}"
-                )
-                raise deployment
-
-        raw_document = documentai.RawDocument(
-            content=image_content, mime_type=mime_type
-        )
