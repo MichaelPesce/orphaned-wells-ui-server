61ab50b1a42f26ea7a529af3b671405e2dc4e040
MichaelPesce <michaelcpesce@gmail.com>
2026-04-16 08:48:52 -0500

    fix logic for adding child fields to parent attributes when the parent attribute has no children yet

--- ogrre/internal/data_manager.py
+++ ogrre/internal/data_manager.py
@@ -1332,14 +1332,38 @@ class DataManager:
                                 "user_added": True,
                                 "topLevelAttribute": parentAttribute,
                             }
-                            data_update = {
-                                "$push": {
-                                    f"attributesList.{primaryIndex}.subattributes": {
-                                        "$each": [newSubField],
-                                        "$position": newSubIndex,
+                            parent_attribute_doc = self.db.records.find_one(
+                                {"_id": _id},
+                                {
+                                    "_id": 0,
+                                    "attributesList": {"$slice": [primaryIndex, 1]},
+                                },
+                            )
+                            parent_attribute = (
+                                parent_attribute_doc.get("attributesList", [None])[0]
+                                if parent_attribute_doc
+                                else None
+                            )
+                            _log.info(f"parent_attribute: {parent_attribute}")
+                            if not parent_attribute:
+                                _log.info(f"Error: tried to insert child attribute to a parent attribute that doesn't exist")
+                                return False
+                            subattributes = parent_attribute.get("subattributes")
+                            if subattributes is not None:
+                                data_update = {
+                                    "$push": {
+                                        f"attributesList.{primaryIndex}.subattributes": {
+                                            "$each": [newSubField],
+                                            "$position": newSubIndex,
+                                        }
                                     }
                                 }
-                            }
+                            else:
+                                data_update = {
+                                    "$set": {
+                                        f"attributesList.{primaryIndex}.subattributes": [newSubField]
+                                    }
+                                }
                     elif update_type == "deleteField":
                         if not isSubattribute:
                             data_update = {
