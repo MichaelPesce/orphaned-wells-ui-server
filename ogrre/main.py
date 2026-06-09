55489ee7b377225d0647a3159fc8cb0b38f52f2e
MichaelPesce <michaelcpesce@gmail.com>
2026-06-09 15:01:32 -0500

    make sure we restart the pod each deployment

--- ogrre/main.py
+++ ogrre/main.py
@@ -47,7 +47,7 @@ app.add_middleware(
     allow_origins=allowed_origins,
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allow_headers=["Authorization", "Content-Type", "X-CSRF-Token"],
+    allow_headers=["*"],
 )
 
 app.include_router(router.router)
