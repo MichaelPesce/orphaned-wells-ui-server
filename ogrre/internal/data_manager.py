d91215b5fc51a4f8d8492d96f5b7781a44584d6b
MichaelPesce <michaelcpesce@gmail.com>
2026-08-18 15:06:58 -0400

    fix record update for verification status

--- ogrre/internal/data_manager.py
+++ ogrre/internal/data_manager.py
@@ -2501,8 +2501,8 @@ class DataManager:
                     )
                 elif update_type == "review_status":
                     data_update["review_status"] = new_data["review_status"]
-                elif update_type == "name":
-                    data_update["name"] = new_data["name"]
+                elif update_type == "name" or update_type == "verification_status":
+                    data_update[update_type] = new_data[update_type]
                 elif update_type != "attributesList":
                     _log.info(f"invalid update type: {update_type}")
                     return False
